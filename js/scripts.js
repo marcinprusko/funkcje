039eab57f5636f59aa4252ce2d7e14294de18201
marcinprusko <marcinprusko@gmail.com>
2017-02-28 22:35:36 +0100

    funkcje

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+﻿// plik scripts.js
+
+function getTriangleArea(a, h) {
+	var triangleArea = a*h/2;
+	if ( a <= 0 || h <= 0 ) {
+		console.log('Nieprawidłowe dane');
+	} 
+	return triangleArea;
+}
+
+getTriangleArea(10,6);
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(15, 25);
+var triangle3Area = getTriangleArea(20, 35);
